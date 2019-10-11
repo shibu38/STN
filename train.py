9f40c0c4a1b3b0f09607d6247508fe5fcb34f649
Shivank <shivank@edisn.ai>
2019-10-11 12:45:39 +0530

    saving model

--- train.py
+++ train.py
@@ -5,57 +5,10 @@ import os
 
 from dataloader import Classifier
 from model import Model
-from utils import save_checkpoint
 
 from torch.utils.tensorboard import SummaryWriter
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-writer = SummaryWriter()
-
-
-# def test(data_loader, model, test_set):
-#     model.eval()
-#     test_loss = 0
-#     correct = 0
-#     for data, target in data_loader:
-#         data, target = data.to(device), target.to(device)
-#         output = model(data)
-#
-#         # sum up batch loss
-#         test_loss += F.nll_loss(output, target, size_average=False).item()
-#         # get the index of the max log-probability
-#         pred = output.max(1, keepdim=True)[1]
-#         correct += pred.eq(target.view_as(pred)).sum().item()
-#
-#     test_loss /= len(data_loader.dataset)
-#     accuracy = 100. * correct / len(data_loader.dataset)
-#     print('\nTest set {} : Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'
-#           .format(test_set, test_loss, correct, len(data_loader.dataset),
-#                   accuracy))
-#     return accuracy
-
-
-# def train(data_loader, model, epoch):
-#     model.train()
-#     batch_idx = 1
-#     total_loss = 0
-#     for images, labels in data_loader:
-#         images = images.to(device)
-#         labels = labels.to(device)
-#         optimizer.zero_grad()
-#         output = model(images)
-#         loss = F.nll_loss(output, labels)
-#         loss.backward()
-#         optimizer.step()
-#         if batch_idx % 10 == 0:
-#             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-#                 epoch, batch_idx * len(images), len(data_loader.dataset),
-#                        100. * batch_idx / len(data_loader), loss.item()))
-#         batch_idx = batch_idx + 1
-#         total_loss = total_loss + loss.item()
-#         writer.add_scalar('Loss/train', loss, epoch * len(data_loader) + batch_idx)
-#     return total_loss
 
 
 class DigitClassifier():
@@ -68,6 +21,8 @@ class DigitClassifier():
         self.learning_rate = learning_rate
         self.model = Model().to(device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        self.training_loss = SummaryWriter()
+        self.validation_acc = SummaryWriter()
 
     def make_dataloader(self, data_dir):
         dataset = Classifier(root_dir=data_dir, width=self.width, height=self.height, grayscale=self.grayscale)
@@ -75,6 +30,17 @@ class DigitClassifier():
                                                  batch_size=self.batch_size,
                                                  shuffle=self.shuffle)
         return dataloader
+
+    def save_model(self, epoch, training_loss, save_path='./saved_models'):
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        filename = os.path.join(save_path, str('ep' + str(epoch) + '.pth.tar'))
+        torch.save({
+            'epoch': epoch,
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'training_loss': training_loss
+        }, filename)
 
     def train_epoch(self, data_loader, epoch):
         self.model.train()
@@ -94,10 +60,10 @@ class DigitClassifier():
                            100. * batch_idx / len(data_loader), loss.item()))
             batch_idx = batch_idx + 1
             total_loss = total_loss + loss.item()
-            writer.add_scalar('Loss/train', loss, epoch * len(data_loader) + batch_idx)
+            self.training_loss.add_scalar('Loss/train', loss, epoch * len(data_loader) + batch_idx)
         return total_loss
 
-    def test(self,data_loader):
+    def test(self, data_loader):
         self.model.eval()
         test_loss = 0
         correct = 0
@@ -116,37 +82,19 @@ class DigitClassifier():
         # print('\nTest set {} : Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'
         #       .format(test_set, test_loss, correct, len(data_loader.dataset),
         #               accuracy))
-        return accuracy,test_loss
+        return accuracy, test_loss
+
+    def train_model(self, num_epoch=100, train_dir='./data/', test_dir='./test1/'):
+        train_dataloader = self.make_dataloader(data_dir=train_dir)
+        test_dataloader = self.make_dataloader(data_dir=test_dir)
+        for epoch in range(1, num_epoch + 1):
+            training_loss = self.train_epoch(train_dataloader, epoch)
+            test_accuracy, test_loss = self.test(test_dataloader)
+            self.validation_acc.add_scalar('Acc/Val', test_accuracy, epoch)
+            self.validation_acc.add_scalar('Loss/Val', test_loss, epoch)
+            self.save_model(epoch, training_loss)
 
 
 if __name__ == "__main__":
-
-# training_data = './data/'
-# test1 = './test1/'
-# test2 = './test2/'
-# width = 28
-# height = 28
-# grayscale = True
-# train_digit_dataset = Classifier(root_dir=training_data, width=width, height=height, grayscale=grayscale)
-# train_data_loader = torch.utils.data.DataLoader(dataset=train_digit_dataset,
-#                                                 batch_size=64,
-#                                                 shuffle=True)
-# test_dataset_1 = Classifier(root_dir=test1, width=width, height=height, grayscale=grayscale)
-# test_loader_1 = torch.utils.data.DataLoader(dataset=test_dataset_1,
-#                                             batch_size=256,
-#                                             shuffle=True)
-# test_dataset_2 = Classifier(root_dir=test2, width=width, height=height, grayscale=grayscale)
-# test_loader_2 = torch.utils.data.DataLoader(dataset=test_dataset_2,
-#                                             batch_size=256,
-#                                             shuffle=True)
-# model = Model().to(device)
-# optimizer = optim.Adam(model.parameters(), lr=0.0001)
-#
-# best_avg_accuracy = 0.0
-# best_training_loss = 0.0
-# for epoch in range(1, 100):
-#     training_loss = train(train_data_loader, model, epoch)
-#     accuracy1 = test(test_loader_1, model, test1)
-#     accuracy2 = test(test_loader_2, model, test2)
-#     if (accuracy1 + accuracy2) / 2 > best_avg_accuracy:
-#         best_avg_accuracy = (accuracy1 + accuracy2) / 2
+    classifier = DigitClassifier()
+    classifier.train_model()
